b8903559d2b915664e702b7ecb71311f3edd0e70
Kimgmltn <Kimgmltn0323@gmail.com>
2021-10-17 22:43:03 +0900

    예제 도메인 모델과 동작확인

---
+++ src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,30 @@
+package study.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id","age"}) // 연관관계인 필드는 ToString에서 안건드리는게 좋다.
+public class Team {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+}
